1d85fb3dcd7caf4c6acc1a5f0c4b42c075083539
Arthur Axel 'fREW' Schmidt <git@frew.co>
2021-02-05 09:39:29 -0800

    put root template in a text file, not in code

--- main.go
+++ main.go
@@ -6,20 +6,8 @@ import (
 )
 
 func main() {
-	root, err := template.New("root").Parse(`
-{{define "begin"}} this is the default begin {{end}}
-{{define "end"}} this is the default end {{end}}
-{{define "inner"}} this is the default inner {{end}}
-
-BEGIN: {{template "begin"}}
-
-INNER: {{template "inner" }}
-
-END: {{template "end"}}`)
-	if err != nil {
-		panic(err)
-	}
-	tpl, err := root.ParseFiles("templates/inner")
+	root := template.New("root")
+	tpl, err := root.ParseFiles("templates/root", "templates/inner")
 	if err != nil {
 		panic(err)
 	}
